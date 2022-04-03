7b76ac8f949d584fd634b3da0ea27bb724bb7b2b
RKZeebon <rkzeebon@gmail.com>
2022-04-03 22:23:54 +0600

    Navber responsived

--- src/Components/Navigation/Navbar.js
+++ src/Components/Navigation/Navbar.js
@@ -1,17 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from "react-router-dom";
+import { MenuIcon, XIcon } from '@heroicons/react/solid'
 import './Navbar.css'
 
 const Navbar = () => {
+    const [open, setOpen] = useState(false)
     return (
         <nav className='navbar'>
             <h1 className='logo'>Symphony</h1>
-            <div>
-                <NavLink className={({ isActive }) => isActive ? 'active-nav-link' : 'nav-link'} to={'/'}>Home</NavLink>
-                <NavLink className={({ isActive }) => isActive ? 'active-nav-link' : 'nav-link'} to={'/reviews'}>Reviews</NavLink>
-                <NavLink className={({ isActive }) => isActive ? 'active-nav-link' : 'nav-link'} to={'/dashboard'}>Dashboard</NavLink>
-                <NavLink className={({ isActive }) => isActive ? 'active-nav-link' : 'nav-link'} to={'/blogs'}>Blogs</NavLink>
-                <NavLink className={({ isActive }) => isActive ? 'active-nav-link' : 'nav-link'} to={'/about'}>About</NavLink>
+            <div className='toggler' onClick={() => setOpen(!open)}>
+                {open ? <XIcon></XIcon> : <MenuIcon> </MenuIcon>}
+            </div>
+            <div className={`${open ? 'nav-links-toggled' : 'nav-links'}`}>
+                <NavLink onClick={() => setOpen(false)} className={({ isActive }) => isActive ? 'active-nav-link' : 'nav-link'} to={'/'}>Home</NavLink>
+                <NavLink onClick={() => setOpen(false)} className={({ isActive }) => isActive ? 'active-nav-link' : 'nav-link'} to={'/reviews'}>Reviews</NavLink>
+                <NavLink onClick={() => setOpen(false)} className={({ isActive }) => isActive ? 'active-nav-link' : 'nav-link'} to={'/dashboard'}>Dashboard</NavLink>
+                <NavLink onClick={() => setOpen(false)} className={({ isActive }) => isActive ? 'active-nav-link' : 'nav-link'} to={'/blogs'}>Blogs</NavLink>
+                <NavLink onClick={() => setOpen(false)} className={({ isActive }) => isActive ? 'active-nav-link' : 'nav-link'} to={'/about'}>About</NavLink>
             </div>
         </nav>
     );
